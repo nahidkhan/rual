0da9137e64e16f8cbe46d4b0ca9ad89dc75d2977
nahidkhan <38406994+nahidkhan@users.noreply.github.com>
2023-01-17 20:49:33 +0600

    hey

---
+++ lib/page.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class firstscreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text("1st tab"),
+      ),
+    );
+  }
+}
+
+class secondscreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text("1st ddddtab"),
+      ),
+    );
+  }
+}
+
+class thirdscreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text("1st dddtab"),
+      ),
+    );
+  }
+}
